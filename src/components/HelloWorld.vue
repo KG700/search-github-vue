0c6ead1a4ce6b951c45c3f6eff2f121b0426629a
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-14 13:52:41 +0100

    Basic connection to github api using axios is set up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,61 +18,35 @@
 
 <script lang="ts">
 import Vue from "vue";
+import axios, { AxiosStatic } from 'axios';
+
+Vue.prototype.$axios = axios;
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $axios: AxiosStatic;
+  }
+}
 
 export default Vue.extend({
   name: "HelloWorld",
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      user: {}
+    }
+  },
+  created () {
+    axios.get(`https://api.github.com/users/${this.username}`)
+      .then(response => {
+        console.log(response.data)
+      })
+  }
 
-  data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
 });
 </script>
